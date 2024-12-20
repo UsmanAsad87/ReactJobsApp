50c41bd14b2edc500ea5508f674153dbfe5d952c
UsmanAsad87 <usmanasad0324@gmail.com>
2024-12-20 23:45:37 +0500

    Use state added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-
+import { useState } from "react" ; 
 const JobListing = ({job}) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+    let description = job.description;
+    if(!showFullDescription){
+        description = description.substring(0,90) + "...";
+    }
   return (
     <div  className="bg-white rounded-xl shadow-md relative">
     <div className="p-4">
@@ -11,7 +15,7 @@ const JobListing = ({job}) => {
         </div>
 
         <div className="mb-5">
-            {job.description}
+            {description}
         </div>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
